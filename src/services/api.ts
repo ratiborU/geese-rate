b2ad635c3a4c0df81a2ee29faca76857e4e5e8f4
ratiborU <tvinck_444@mail.ru>
2025-01-21 21:07:18 +0500

    feat: auth

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,35 +1,34 @@
 import axios from 'axios';
-// import { UserSecretStorageService } from '../lib/helpers/userSecretStorageService';
-// import { AuthService } from './authService';
-// console.log(import.meta.env);
+import { UserSecretStorageService } from '../lib/helpers/userSecretStorageService';
+import { AuthService } from './authService';
+
+
 const $http = axios.create({
-  // baseURL: 'http://127.0.0.1:8000/api'
-  // baseURL: process.env.REACT_APP_BACKEND_URL
   baseURL: import.meta.env.VITE_BACKEND_URL
 });
 
-// $http.interceptors.response.use(
-//   (config) => config,
-//   async (error) => {
-//     const originalRequest = error.config;
-//     if (error.response.status === 401 && !originalRequest._isRetry) {
-//       if (originalRequest.url == '/token/refresh/') {
-//         window.location.href = '/login';
-//       }
-//       originalRequest._isRetry = true;
-//       await AuthService.refresh();
-//       return $http.request(originalRequest);
-//     }
-//     throw error;
-//   },
-// );
+$http.interceptors.response.use(
+  (config) => config,
+  async (error) => {
+    const originalRequest = error.config;
+    if (error.response.status === 401 && !originalRequest._isRetry) {
+      if (originalRequest.url == '/token/refresh/') {
+        window.location.href = '/';
+      }
+      originalRequest._isRetry = true;
+      await AuthService.refresh();
+      return $http.request(originalRequest);
+    }
+    throw error;
+  },
+);
 
-// $http.interceptors.request.use((config) => {
-//   const token = UserSecretStorageService.get()?.access;
-//   if (token) {
-//     config.headers['Authorization'] = `Bearer ${token}`;
-//   }
-//   return config;
-// });
+$http.interceptors.request.use((config) => {
+  const token = UserSecretStorageService.get()?.access;
+  if (token) {
+    config.headers['Authorization'] = `Bearer ${token}`;
+  }
+  return config;
+});
 
 export default $http;
